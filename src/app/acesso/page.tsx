07d06c4aba8974fe37ae0d5feef791a5531d2f1b
Gabriel <gabrielmoura@MacBook-Air-de-Gabriel-2.local>
2025-05-17 18:57:22 -0300

    feat: responsivness

--- src/app/acesso/page.tsx
+++ src/app/acesso/page.tsx
@@ -4,7 +4,7 @@ import LoginText from "./components/LoginText";
 export default function AcessoPage() {
   return (
     <div
-      className="min-h-screen w-full px-[89px] py-[205px] max-xl:px-[3rem] flex justify-between"
+      className="min-h-screen w-full px-[89px] py-[205px] max-xl:px-[3rem] flex flex-col items-center md:flex-row justify-between"
       style={{
         backgroundImage: "url('/assets/images/bg-arquivo.png')",
         backgroundSize: "cover",
