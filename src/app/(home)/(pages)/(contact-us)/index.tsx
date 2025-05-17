097c13c305f6d591c35a3bd2665e8b075c44368f
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-05-17 20:06:08 -0300

    fix: projects pages responsivines

--- src/app/(home)/(pages)/(contact-us)/index.tsx
+++ src/app/(home)/(pages)/(contact-us)/index.tsx
@@ -10,8 +10,8 @@ const ContactUs: React.FC = () => {
       className="min-h-screen bg-red-500 flex justify-center"
       id="contact"
     >
-      <div className="flex gap-40 w-[1280px] justify-center items-center px-10">
-        <div className="w-full">
+      <div className="flex flex-col-reverse lg:flex-row gap-40 w-[1280px] justify-center items-center px-10 py-30 lg:py-0">
+        <div className="w-full max-sm:w-fit">
           <h2 className="text-d-xl font-bold tracking-tight text-white">
             fale conosco
           </h2>
